58ba97ad462c2f1e7694cf6828a30ef20323c635
Sami-Peeter Laitinen <samipeeter.k@gmail.com>
2018-03-23 10:33:42 +0200

    Add files via upload

---
+++ Harjoitus 1/connect4.py
@@ -0,0 +1,282 @@
+# -*- coding: utf-8 -*-
+	
+import copy
+
+class ConnectFour:
+    def __init__(self):
+        # Alustetaan liikkeiden määrä nollaan (42 liikkeen jälkeen lauta on täynnä) ja kenen vuoro on algoritmissa
+        self.moves = 0
+        self.turn = 0
+
+    def PrintGameBoard(self, board):
+        # Tämä funktio piirtää pelilaudan
+        print('  0   1   2   3   4   5   6')
+        for i in range(5, -1, -1):
+            print('|---|---|---|---|---|---|---|')
+            print("| ",end="")
+            for j in range(7):
+                print(board[i][j],end="")
+                if j != 6:
+                    print(" | ",end="")
+                else:
+                    print(" |")
+        print('`---------------------------´')
+
+    def LegalRow(self, col, board):
+        # Tämä funktio tarkistaa sarakkeella jo olevien merkkien määrän ja palauttaa mille riville uusi merkki menee. Jos sarake on täynnä funktio palauttaa arvon -1
+        stacks = [[x[i] for x in board] for i in range(len(board[0]))]
+        countofitems = stacks[col].count("x") + stacks[col].count("o")
+        if (countofitems) < 6:
+            return (countofitems)
+        else:
+            return -1
+			
+    def LegalMoves(self, board):
+        # Tämä funktio palauttaa lailliset siirrot listassa tietyssä järjestyksessä
+        legalmoves = []
+        stacks = [[x[i] for x in board] for i in range(len(board[0]))] 
+        order = [3,2,4,1,5,0,6]
+        for i in order:
+            if self.LegalRow(i, board)!=-1:
+                legalmoves.append(i)
+        return legalmoves
+
+    def MakeMove(self, board, col, player, row):
+        # Tämä funktio tekee siirron ja kasvattaa tehtyjen siirtojen lukumäärää yhdellä
+        board[row][col] = player
+        self.moves += 1
+        return board
+		
+    def UnmakeMove(self, board, col, row):
+        # Tämä funktio korvaa aiemmin tehdyn siirron tyhjällä ja pienentää tehtyjen siirtojen lukumäärää yhdellä
+        board[row][col] = " "
+        self.moves -= 1
+        return board
+
+    def IsWinning(self, currentplayer, board):
+        # Tämä funktio tarkistaa onko pelilaudalla vuorossa olevalla pelaajalla neljä perättäistä merkkiä peräkkäin
+        for i in range(6):
+            for j in range(4):
+                if board[i][j] == currentplayer and board[i][j+1] == currentplayer and board[i][j+2] == currentplayer and board[i][j+3] == currentplayer:
+                    return True
+        for i in range(3):
+            for j in range(7):
+                if board[i][j] == currentplayer and board[i+1][j] == currentplayer and board[i+2][j] == currentplayer and board[i+3][j] == currentplayer:
+                    return True     
+        for i in range(3):
+            for j in range(4):
+                if board[i][j] == currentplayer and board[i+1][j+1] == currentplayer and board[i+2][j+2] == currentplayer and board[i+3][j+3] == currentplayer:
+                    return True
+        for i in range(3,6):
+            for j in range(4):
+                if board[i][j] == currentplayer and board[i-1][j+1] == currentplayer and board[i-2][j+2] == currentplayer and board[i-3][j+3] == currentplayer:
+                    return True
+        return False
+		               
+    def PlayerMove(self, board, player):
+        # Tämä funktio pyytää pelaajan tekevän liikkeen ja palauttaa uuden pelilaudan tehdyn liikkeen jälkeen
+        allowedmove = False
+        while not allowedmove:
+            try:
+                print("Choose a column where you want to make your move (0-6): ",end="")
+                col =input()
+                col=int(col)
+                row = self.LegalRow(col, board)
+            except (NameError, ValueError, IndexError, TypeError, SyntaxError) as e:
+                print("Give a number as an integer between 0-6!")
+            else:
+                if row != -1 and (col<=6 and col>=0):
+                    board[row][int(col)] = player
+                    self.moves += 1
+                    allowedmove = True
+                elif col>6 or col<0:
+                    print("The range was 0-6!!!")
+                else:
+                    print("This column is full")
+        return board
+			
+    def SwitchPlayer(self, player):
+        # Tämä funktio vaihtaa vuoroa tarkastamalla nykyisen pelaajan merkin ja palauttamalla seuraavan pelaajan merkin
+        if player=="x":
+            nextplayer="o"
+        else:
+            nextplayer="x"
+        return nextplayer
+			
+    def evaluation(self, board):
+        # Tämä funktio laskee pelilaudalle arvion ohjeiden mukaan. 
+        list = []
+        player = "x"
+        opponent = "o"
+        if self.IsWinning(player, board):
+            score = -512
+        elif self.IsWinning(opponent, board):
+            score = +512
+        elif self.moves==42:
+            score=0
+        else:
+            score = 0
+            for i in range(6):  # Lisätään listaan vaakasuuntaiset segmentit
+                for j in range(4):
+                    list.append([str(board[i][j]),str(board[i][j+1]),str(board[i][j+2]),str(board[i][j+3])])
+            for i in range(3): # Lisätään listaan pystysuuntaiset segmentit
+                for j in range(7):
+                    list.append([str(board[i][j]),str(board[i+1][j]),str(board[i+2][j]),str(board[i+3][j])])
+            for i in range(3): # Lisätään listaan diagonaaliset segmentit
+                for j in range(4):
+                    list.append([str(board[i][j]),str(board[i+1][j+2]),str(board[i+2][j+2]),str(board[i+3][j+3])])
+            for i in range(3, 6): # Lisätään listaan diagonaaliset segmentit
+                for j in range(4):
+                    list.append([str(board[i][j]),str(board[i-1][j+2]),str(board[i-2][j+2]),str(board[i-3][j+3])])
+            for k in range(len(list)): # Pisteytetään segmentit ohjeiden mukaisesti
+                if ((list[k].count(str("x"))>0) and (list[k].count(str("o"))>0)) or list[k].count(" ")==4:
+                    score += 0
+                if list[k].count(player)==1 and list[k].count(opponent)==0:
+                    score -= 1
+                if list[k].count(player)==2 and list[k].count(opponent)==0:
+                    score -= 10
+                if list[k].count(player)==3 and list[k].count(opponent)==0:
+                    score -= 50
+                if list[k].count(opponent)==1 and list[k].count(player)==0:
+                    score += 1
+                if list[k].count(opponent)==2 and list[k].count(player)==0:
+                    score += 10
+                if list[k].count(opponent)==3 and list[k].count(player)==0:
+                    score += 50
+            if self.turn==player:
+                score -= 16
+            else:
+                score += 16
+        return score
+		      
+    def alphabetapruning(self, board, depth, opponent, alpha, beta):
+        #Funktion parametrit:
+		#
+		#self: yläluokan ConnectFour kaikki parametrit
+		#board: pelilauta 6x7 matriisina
+		#depth: etsintäsyvyys (älä valitse liian suurta etsintäsyvyyttä, koska laskenta saattaa kestää liian kauan)
+        #opponent: vastustajan merkki
+        #alpha: alphan numeroarvo
+        #beta: betan numeroarvo
+        #
+		#Funktio palauttaa:
+		#largestvalue, position: suurimman arvon, johon päästään sijoittamalla pelimerkki johonkin sarakkeista ja sarakkeen numero
+        values = []
+        cols = []
+		#########################################################################
+		#SINUN KOODISI TÄHÄN
+		#########################################################################
+								
+    def minimax(self, board, depth, opponent):
+		#Funktion parametrit:
+		#
+		#self: yläluokan ConnectFour kaikki parametrit
+		#board: pelilauta 6x7 matriisina
+		#depth: etsintäsyvyys (älä valitse liian suurta etsintäsyvyyttä, koska laskenta saattaa kestää liian kauan)
+        #opponent: vastustajan merkki
+        #
+		#Funktio palauttaa:
+		#largestvalue, position: suurimman arvon, johon päästään sijoittamalla pelimerkki johonkin sarakkeista ja sarakkeen numero
+        values = []
+        cols = []
+		#########################################################################
+		#SINUN KOODISI TÄHÄN
+		#########################################################################
+        for node in self.LegalMoves(board):
+            if depth == 0 or self.moves == 42:
+                return self.evaluation(board), node
+            
+            if opponent == "x": # max
+                bestValue = float('-inf')
+                newBoard = self.MakeMove(board, node, "o", self.LegalRow(node, board))
+                v, position = self.minimax(newBoard, depth, "o")
+                if v > bestValue:
+                    bestValue = v
+                else:
+                    position = node
+
+            else:           # Minimizer
+                bestValue = float('inf')
+                newBoard = self.MakeMove(board, node, "x", self.LegalRow(node, board))
+                v, position = self.minimax(newBoard, depth - 1, "x")
+                if v < bestValue:
+                    bestValue = v
+                else:
+                    position = node
+            print(bestValue)
+            print(position)
+            return bestValue, position
+					
+    def searchingfunction(self, board, depth, opponent):
+        # Tämä funktio kutsuu alpfabeta-pruning (tai minimax) algoritmia, jonka jälkeen päivittää pelilaudan saadulla liikkeella ja palauttaa uuden pelilaudan
+        newboard = copy.deepcopy(board)
+        #value, position = self.alphabetapruning(newboard, depth, opponent, -1000000000, 1000000000)
+        value, position=self.minimax(newboard, depth, opponent)
+        board = self.MakeMove(board, position, opponent, self.LegalRow(position, board))
+        return board
+
+def PlayerGoesFirst():
+    # Tämä funktio kysyy kumpi pelaajista aloittaa. Pelaaja on X ja tietokone on O
+    print("Player is X and AI is O")
+    player = 'x'
+    opponent = 'o'
+    print('Do you want to play first? (y/n) : ',end="")
+    return input().lower().startswith('y')
+    
+def PlayAgain():
+    # Tämä funktio kysyy pelin päätyttyä pelataanko uudestaan
+    print('Do you want to play again? (y/n) :',end="")
+    return input().lower().startswith('y')
+	
+def main():
+    # Pääfunktio
+    print("Connect4")
+    print("-"*34)
+    while True:
+        connectfour = ConnectFour()
+        gameisgoing = True
+        table  = [[],[],[],[],[],[]]
+        for i in range(7):
+            for j in range(6):
+                table[j].append(" ")
+        player = "x"
+        opponent = "o"
+        if PlayerGoesFirst():
+            turn = "x"
+        else:
+            turn = "o"
+        while gameisgoing:
+            connectfour.PrintGameBoard(table)
+            if turn=="x":
+                table = connectfour.PlayerMove(table, player)
+                if connectfour.IsWinning(player, table):
+                    connectfour.PrintGameBoard(table)
+                    print('You won the game!')
+                    gameisgoing = False
+                else:
+                    if connectfour.moves==42:
+                        connectfour.PrintGameBoard(table)
+                        print('Game is tie')
+                        gameisgoing=False
+                    else:
+                        turn = "o"
+            else:
+                table = connectfour.searchingfunction(table, 6, opponent)# Tällä rivillä tapahtuu AI:n liike joko minimax algoritmilla tai alphabeta-pruning algoritmilla
+                if connectfour.IsWinning(opponent, table):
+                    connectfour.PrintGameBoard(table)
+                    print('Computer won the game')
+                    gameisgoing = False
+                else:
+                    if connectfour.moves==42:
+                        connectfour.PrintGameBoard(table)
+                        print('Game is tie')
+                        gameisgoing=False
+                    else:
+                        turn = "x"
+        if not PlayAgain():
+            print("Game ended")
+            print("-"*34)
+            break
+
+if __name__ == '__main__':
+    main()
---
+++ Harjoitus 1/eightpuzzle.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+import time
+import queue
+import random
+
+class EightPuzzle:
+    def __init__(self):
+        # Tässä funktiossa alustetaan 3x3 lauta listana
+        self.targetstate = list(range(1, 9))
+        self.targetstate.append(0)
+
+    def PrintState(self, state):
+        # Tämä funktio printtaa lista matriisi formaatissa
+        for (index, value) in enumerate(state):
+            print("{}".format(value),end="")
+            if index==2 or index==5 or index==8:
+                print("")
+        print("")
+
+    def LegalMoves(self, position):
+        # Tämä funktio ratkaisee laillisten siirtojen paikan matriisissa
+        values = [1, -1, 3, -3]
+        legalmoves = []
+        for x in values:
+            if 0 <= position + x < 9:
+                if x == 1 and (position==2 or position==5 or position==8):
+                    continue
+                if x == -1 and (position==0 or position==3 or position==6):
+                    continue
+                legalmoves.append(x)
+        return legalmoves
+
+    def FindNextStates(self, state):
+        # Tämä funktio ratkaisee seuraavat mahdolliset siirrot nykyisestä tilasta
+        legalmoves = {}
+        for position in range(9):
+            legalmoves[position] = self.LegalMoves(position)
+        emptyspace = state.index(0)
+        moves = legalmoves[emptyspace]
+        nextstates = []
+        for move in moves:
+            state_x = state[:]
+            (state_x[emptyspace + move], state_x[emptyspace]) = (state_x[emptyspace], state_x[emptyspace + move])
+            nextstates.append(state_x)
+        return nextstates
+
+    def DrawState(self, state):
+        # Tämä funktio valitsee seuraavan tilan sattumanvaraisesti. Tätä funktiota tarvitaan sekoittaessa peliä
+        nextstates = self.FindNextStates(state)
+        randomstate = random.choice(nextstates)
+        return randomstate
+
+    def InitialState(self, shuffles=200):
+        # Tämä funktio ratkaisee alkutilan pelille. Sekoitetaan 200 kertaa mikäli sekoitusten määrää ei ole annettu etukäteen
+        initialstate = (self.targetstate)[:]
+        for i in range(shuffles):
+            initialstate = self.DrawState(initialstate)
+        return initialstate
+
+    def GameSolved(self, state):
+        # Tämä funktio tarkistaa, onko peli ratkaistu
+        return state == self.targetstate
+		
+    def Manhattan(self, state):
+        # Tämä funktio laskee manhattan etäisyyden nykyiselle tilalle. Manhatten etäisyys lasketaan lisäämällä kaikkiin numeroihin (1-8) horisontaaliset ja vertikaaliset liikkeet saadakseen numero oikealle paikalle.
+        manhattanvalue = 0
+        for number in state:
+            if number != 0:
+                greedyvalue = abs(self.targetstate.index(number) - state.index(number))
+                (verticalvalue, horizontalvalue) = (greedyvalue // 3, greedyvalue % 3)
+                manhattanvalue += verticalvalue + horizontalvalue
+        return manhattanvalue
+		
+    def Hamming(self, state):
+        # Tämä funktio laskee kuinka moni laatoista on väärällä paikalla (Hamming etäisyys)
+        hamming = 0
+        for i in range(9):
+            if state[i] != 0 and state[i] != self.targetstate[i]:
+                hamming += 1
+        return hamming
+		
+    def GetLowestF(self, openlist, fscore):
+        # Tämä funktio tarkistaa avoimen listan pienimmän f-scoren arvon ja palauttaa kyseisen tilan
+        value = 10000000000
+        for state in openlist:
+            if fscore[self.ListToString(state)] < value:
+                value = fscore[self.ListToString(state)]
+                current = state
+        return current
+		
+    def ListToString(self, numberlist):
+        # Tämä funktio muuttaa listan stringiksi
+        return ''.join(list(map(str, numberlist)))
+	
+    def ReconstructPath(self, camefrom, current):
+        # Tämä funktio rakentaa polun alkutilasta tavoitetilaan, kun tavoitetila on löydetty
+        totalpath = []
+        current = self.targetstate
+        while self.ListToString(current) in list(camefrom.keys()):
+            current = camefrom[self.ListToString(current)]
+            totalpath.append(current)
+        totalpath.reverse()
+        totalpath.append(self.targetstate)
+        for i in totalpath:
+            self.PrintState(i)
+        print("Used total {} moves".format(len(totalpath)-1))
+                    
+    def Astar(self, state):
+        #Funktion parametrit:
+        #
+        #self: yläluokan EightPuzzle kaikki parametrit
+        #state: tila, joka yritetään ratkaista
+        #
+        #Funktio palauttaa:
+        #
+        #self.ReconstructPath(camefrom, state): lyhimmän mahdollisen polun, jonka avulla päästää tavoitetilaan
+        camefrom = {}
+        openlist = []
+        closedlist = []
+        gscore = {}
+        fscore = {}
+        openlist.append(state)
+        gscore[self.ListToString(state)] = 0
+        fscore[self.ListToString(state)] = gscore[self.ListToString(state)] + self.Manhattan(state)
+        ######################################################################### 
+        #SINUN KOODISI TÄHÄN
+        #########################################################################
+        
+		
+def main():
+    print('8-Puzzle Solver!')
+    print(15 * '-')
+    game = EightPuzzle()
+    print('The starting state is:')
+    initialstate = game.InitialState(50)
+    game.PrintState(initialstate)
+    print('The target state will be:')
+    game.PrintState(game.targetstate)
+    print('The path of solution:')
+    game.Astar(initialstate)
+	
+if __name__ == '__main__':
+    main()
